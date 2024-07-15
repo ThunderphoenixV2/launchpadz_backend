fb6934f0870b244d5db82b50e7dedc830b3b3015
Thunder <153645532+ThunderphoenixV2@users.noreply.github.com>
2024-07-16 01:13:36 +0200

    removed unused SQL tables

--- src/schema.rs
+++ src/schema.rs
@@ -1,21 +1,9 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    user (id) {
-        id -> Int4,
-        name -> Varchar,
-    }
-}
-
-diesel::table! {
     users (id) {
         id -> Int4,
         name -> Varchar,
         email -> Text,
     }
 }
-
-diesel::allow_tables_to_appear_in_same_query!(
-    user,
-    users,
-);
